d294cb44de6e9103315f67cc968ebd04a4fdc938
virinci <revirinci@proton.me>
2024-10-02 13:19:25 +0530

    Implement the load balancer listener and the logic

--- main.go
+++ main.go
@@ -32,6 +32,7 @@ func main() {
 	log.Println(serverAddr)
 
 	port := getEnvOr("PORT", "3030")
+
 	listener, err := net.Listen("tcp", ":"+port)
 	if err != nil {
 		log.Fatal(err)
@@ -39,6 +40,10 @@ func main() {
 	defer listener.Close()
 	log.Println("TCP listener initialized at", listener.Addr())
 
+	BalanceLoad(listener, strategy)
+}
+
+func BalanceLoad(listener net.Listener, strategy Strategy) {
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
@@ -46,13 +51,28 @@ func main() {
 			continue
 		}
 
-		go func(conn net.Conn) {
-			_, err := io.Copy(conn, conn)
+		go func(conn1 net.Conn) {
+			serverAddr, err := strategy.ServerAddr()
 			if err != nil {
-				log.Println(err)
+				log.Println("no servers to forward to:", err)
+				return
 			}
+
+			conn2, err := net.Dial("tcp", string(serverAddr))
+			if err != nil {
+				log.Println("couldn't connect to the selected server", serverAddr)
+				return
+			}
+
+			strategy.Connected(serverAddr)
+
+			go io.Copy(conn2, conn1)
+			go io.Copy(conn1, conn2)
+
 			// Close the connection and decrement connection count.
-			conn.Close()
+			conn1.Close()
+			conn2.Close()
+			strategy.Disconnected(serverAddr)
 		}(conn)
 	}
 }
